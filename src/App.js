78e42c564b92bda25f282243e0968f1ef295a3d4
Oyekunletomide <Oyekunletomide@gmail.com>
2023-01-22 21:25:00 +0530

    added meta tags and page semantic elements components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Nav from './Nav';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div>Homepage</div>
+    <>
+      <Header />
+
+      <Nav />
+
+      <Main />
+
+      <Footer />
+      Homepage
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,39 @@
+
+
+
+export default function Footer(){
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <a href="#Home">Home</a>
+                </li>
+                <li>
+                    <a href="#About">About</a>
+                </li>
+                <li>
+                    <a href="#Menu">Menu</a>
+                </li>
+                <li>
+                    <a href="#Reservations">Reservations</a>
+                </li>
+                <li>
+                    <a href="#OrderOnline">Order Online</a>
+                </li>
+                <li>
+                    <a href="#Login">Login</a>
+                </li>
+            </ul>
+            <div>
+                <p>Address</p>
+                <p>Phone Number</p>
+                <p>Email</p>
+            </div>
+            <div>
+                <p>Address</p>
+                <p>Phone Number</p>
+                <p>Email</p>
+            </div>
+        </footer>
+    )
+}
